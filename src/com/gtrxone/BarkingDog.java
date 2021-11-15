69a8491b2f27aa366e2142050c59c542d3dda1a2
Goonawardana R N W <gtrxone@gmail.com>
2021-11-15 23:14:33 +0530

    Initial commit

---
+++ src/com/gtrxone/BarkingDog.java
@@ -0,0 +1,12 @@
+package com.gtrxone;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay >= 0 && hourOfDay <= 23) {
+            return (barking && hourOfDay < 8 || hourOfDay > 22);
+
+        } else
+            return false;
+
+    }
+}
